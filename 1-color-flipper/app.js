7c23980af0402c2b92a56259a682b1860794f524
Igor Basuga <igor.basuga@gmail.com>
2020-08-04 10:44:54 +0200

    complete simple setup

--- 1-color-flipper/app.js
+++ 1-color-flipper/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colours = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const button = document.getElementById("btn");
+const colour = document.querySelector("colour");
+
+button.addEventListener("click", function () {
+  // get random number between 0 and 3
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colours[randomNumber];
+  colours.textContent = colours[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colours.length);
+}
