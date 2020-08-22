90737f0edc4e41d56baf78557ff4aa542a075abf
Igor Basuga <igor.basuga@gmail.com>
2020-08-22 16:39:36 +0200

    complete Project 11

---
+++ 11-tabs/app.js
@@ -0,0 +1,18 @@
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const el = document.getElementById(id);
+    el.classList.add("active");
+  }
+});
